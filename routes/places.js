0a5f8a5d897ffbb04b081cfe9921c7ac49df2d49
Ralf Lici <slonebrownstone@gmail.com>
2021-01-29 00:13:38 +0100

    added first post function

--- routes/places.js
+++ routes/places.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var place_controller = require("../controllers/placeController");
 
 router.get("/", place_controller.places_list);
+router.post("/price", place_controller.places_list);
 
 module.exports = router;
