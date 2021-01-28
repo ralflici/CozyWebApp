06f605f08dd20233c63d221114e578ee1e61a5e9
Ralf Lici <slonebrownstone@gmail.com>
2021-01-28 18:04:48 +0100

    added post methods

--- controllers/locationController.js
+++ controllers/locationController.js
@@ -8,3 +8,25 @@ exports.locations_list = function(req, res) {
             res.send(listLocations);
         });
 }
+
+exports.locations_name = function(req, res) {
+    const name = req.body.location;
+    const regexp = new RegExp(name, "i");
+    Location
+        .find({name: {$regex: regexp}})
+        .exec(function (err, listLocations) {
+            if (err) {console.log(err);}
+            res.send(listLocations);
+        })
+}
+
+exports.locations_continent = function(req, res) {
+    const continent = req.body.continent;
+    const regexp = new RegExp(continent, "i");
+    Location
+        .find({continent: {$regex: regexp}})
+        .exec(function (err, listLocations) {
+            if (err) {console.log(err);}
+            res.send(listLocations);
+        })
+}
