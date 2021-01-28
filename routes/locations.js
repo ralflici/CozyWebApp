e6b3ee0ef6d121da63acce774efa67469ed3c10f
Ralf Lici <slonebrownstone@gmail.com>
2021-01-28 18:02:15 +0100

    added search methods

--- routes/locations.js
+++ routes/locations.js
@@ -3,5 +3,7 @@ var router = express.Router();
 var location_controller = require("../controllers/locationController");
 
 router.get("/", location_controller.locations_list);
+router.post("/name", location_controller.locations_name);
+router.post("/continent", location_controller.locations_continent);
 
 module.exports = router;
