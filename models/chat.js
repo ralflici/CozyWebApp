946485c580debc0e7300f87e39dcd23b21535bf4
Ralf Lici <slonebrownstone@gmail.com>
2021-02-07 16:17:52 +0100

    solving routes issues

--- models/chat.js
+++ models/chat.js
@@ -7,7 +7,7 @@ var chatSchema = new Schema({
     content: [{
         sender: {type: String, required: true, enum: ["user", "place"]},
         //read: {type: Boolean, required: true},
-        message: {type: String, required: true, maxlength: 200},
+        message: {type: String, required: true, maxlength: 500},
         date: {type: Date, required: true}
     }]
 });
